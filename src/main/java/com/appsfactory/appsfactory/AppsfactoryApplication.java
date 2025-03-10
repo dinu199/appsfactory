86ebd99b57e8843f95a61d94a7905a89c4266892
Dinu Donoaga <90680928+dinu199@users.noreply.github.com>
2025-03-10 19:12:00 +0200

    Add files via upload

---
+++ src/main/java/com/appsfactory/appsfactory/AppsfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.appsfactory.appsfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsfactoryApplication.class, args);
+	}
+
+}
